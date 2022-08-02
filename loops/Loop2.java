6e0169cfe9df27fbae9ab50d121d9bd2d14f8309
Partha Sarathi Manna <66555692+04psm@users.noreply.github.com>
2022-08-02 20:43:47 +0530

    Add files via upload

---
+++ loops/Loop2.java
@@ -0,0 +1,50 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		//Fibonacci number  
+		Scanner sc = new Scanner(System.in);
+		
+//		a b c 
+//		0 1 1
+//		1 1 2
+//		1 2 3
+//		2 3 5
+//		3 5 8
+//		5 8 13
+		
+//		int n = sc.nextInt();
+//		int a = 0 ; 
+//		int b = 1 ; 
+//		System.out.print(a+" "+ b +" ");
+//		for(int i = 0 ;i<n-2;i++) {
+//		 int c = a + b ;
+//		 System.out.print(c +" ");
+//		 a = b;
+//		 b = c ; 
+//		}
+		
+		
+	
+////		power  or  the built in methods is Math.pow()
+//		System.out.println("Enter your num");
+//		int a = sc.nextInt();
+//		System.out.println("Enter the power");
+//		int b = sc.nextInt();
+//		int power = 1; 
+//		// you can run the loop from i = 0 to b OR i = 1 to i<=b both are same 
+//		for(int i = 1 ; i<=b;i++) {
+//			power *= a;
+//		}
+//		System.out.println(a +" to the power "+ b+" is : "+power);
+//		
+////		System.out.println(Math.pow(a, b)); //this is the built in methods
+		
+		
+		// prime number or not 
+	}
+
+}
