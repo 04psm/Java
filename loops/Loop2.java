ed2fb68159e11ccf7a70d35914a0a3569cc546e5
Partha Sarathi Manna <66555692+04psm@users.noreply.github.com>
2022-08-03 12:38:26 +0530

    Update Loop2.java

--- loops/Loop2.java
+++ loops/Loop2.java
@@ -44,7 +44,60 @@ public class Loop2 {
 ////		System.out.println(Math.pow(a, b)); //this is the built in methods
 		
 		
-		// prime number or not 
+//		// prime number or not 
+//		//What is prime number 
+//		// a number that is divisible only by itself and 1
+//		int n = sc.nextInt();
+//		boolean isPrime = true;
+//		
+//		if(n<2) isPrime=false;
+//		
+//		for(int i = 2 ; i*i<n;i++) {
+////			System.out.println(i);
+//				if(n % i == 0) {
+//					isPrime = false ;
+//					break;
+//				}
+//		}
+//		System.out.println("is Prime :"+isPrime);
+		
+		
+		
+		
+//		//1 + 1/2 + 1/3 + 1/4 ... 1/n
+//		int n = sc.nextInt();
+//		float r = 0 ; 
+//		for(float i = 1 ; i<=n;i++) {
+//			r += 1/i;
+//			System.out.println(r);
+//		}
+//		
+		
+//		1-1/2+1/3-1/4+1/5...1/n
+//		int n = sc.nextInt();
+//		float result = 0 ; 
+//		for(float i = 1 ;i<=n;i++) {
+//			if(i%2==0) {
+//				result -= 1/i ; 
+//			}else {
+//				result += 1/i ; 
+//			}
+//		}
+//		System.out.println(result);
+		
+		
+//	 // Break 
+//		for(int i = 0 ;i<=10;i++) {
+//			if(i==5) break;
+//			System.out.println(i);
+//		}
+		
+	// Continue 
+//		for(int i = 0 ; i<=10;i++) {
+//			if(i>=5 && i<=9) continue;
+//			System.out.println(i); 
+//		}
+		
 	}
 
 }
