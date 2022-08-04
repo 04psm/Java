13325a63d0d78211b91867ad16677f09cd8ddf58
Partha Sarathi Manna <66555692+04psm@users.noreply.github.com>
2022-08-04 10:19:59 +0530

    Add files via upload

---
+++ loops/Patterns.java
@@ -0,0 +1,111 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Patterns {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		
+/*
+              *  
+            * * *  
+          * * * * *  
+        * * * * * * *  
+      * * * * * * * * *  
+    * * * * * * * * * * *  
+  * * * * * * * * * * * * *  
+* * * * * * * * * * * * * * *  
+ 
+ */
+		
+//		int n = sc.nextInt();
+//		for(int i = 1 ; i<=n;i++) {
+//			for(int j = 1 ;j<=n-i;j++) {
+//				System.out.print("  ");
+//			}
+//			for(int j =1 ; j<= 2*i-1 ; j++) {//j<=i
+//				System.out.print("* ");
+//			}
+//			System.out.println(" ");
+//		}
+	
+	
+	
+//		
+//		       1  
+//           2  3  
+//         4  5  6  
+//       7  8  9  10  
+		
+		
+//	int n = sc.nextInt();
+//	int num = 1 ; 
+//	for(int i = 1 ; i<=n;i++) {
+//		for(int j = 1 ;j<=n-i;j++) {
+//			System.out.print("  ");
+//		}
+//		for(int j =1 ; j<=i ; j++) {//j<=i
+//			System.out.print(num++ +"  ");
+//		}
+//		System.out.println();
+//	}
+		
+//		* 
+//		* * 
+//		* * * 
+//		* * * * 
+//		* * * * * 
+//		* * * * 
+//		* * * 
+//		* * 
+//		* 
+
+		
+//		int n = sc.nextInt();
+//		int row = 2*n-1;
+//		
+//		for(int i = 1 ;i<=row ; i++) {
+//			if(i<=n) {
+//				for(int j = 1; j<=i;j++) {
+//					System.out.print("* ");
+//				}
+//			}else {
+//				for(int j = 1 ;j<=row-i+1;j++) {
+//					System.out.print("* ");
+//				}
+//			}
+//			System.out.println();
+//		}
+		
+		
+		int n = sc.nextInt();
+		
+		System.out.println("*");
+		
+		for(int i = 2 ;i<=n-1;i++) {
+			System.out.print("* ");
+			for(int j = 1 ; j<=i-2;j++) {
+				System.out.print( "  " );
+			}
+			System.out.print("* ");
+			System.out.println();
+		}
+		
+		if(n>1) {			
+			for(int l = 1 ; l<=n;l++) {
+				System.out.print("* ");
+			}
+		}
+		
+		
+		
+		
+	}
+
+}
+
+
+
