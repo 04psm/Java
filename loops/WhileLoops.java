a62f506a88950e4696d40882ad199ee98dc2ca40
Partha Sarathi Manna <66555692+04psm@users.noreply.github.com>
2022-08-05 15:10:14 +0530

    While loop and do while

---
+++ loops/WhileLoops.java
@@ -0,0 +1,53 @@
+package loops;
+
+import java.util.Scanner;
+
+public class WhileLoops {
+public static void main(String[] args) {
+	// sum of digits 
+	Scanner sc = new Scanner(System.in);
+//	int n = sc.nextInt();
+//	int temp = n ; 
+//	int sum = 0 ; 
+//	while(temp>0) {
+//		int lastDigit = temp % 10 ; 
+//		//System.out.println("lastDigit : " + lastDigit);
+//	//	System.out.println("before div by 10 temp : " + temp);
+//		temp = temp / 10 ; 
+//		//System.out.println(" after div by 10 temp : " + temp);
+//		sum = sum + lastDigit;
+//	//System.out.println(" the last digit is  "+ lastDigit + " , in temp : " + temp + " , sum : " + sum);	// in side the loop  
+//	}
+//	System.out.println(n + " sum is "+sum);
+//	
+	
+	
+	// palindrome 
+//	int n = sc.nextInt();
+//	int temp  = n ; 
+//	int r =  0 ; 
+//	while(temp>0) {
+//		int lastDigit = temp % 10 ; 
+//		r = r *10+lastDigit;
+//		temp /=10;
+//	}
+//	System.out.println(r);
+//	if(r == n) {
+//		System.out.println(n +"this is a palindrome");
+//	}else {
+//		System.out.println(n + "this is not a plindrome");
+//	}
+//	
+	
+	
+	// do-while loop
+//	int n = 1; 
+//	do {
+//		 n = sc.nextInt();
+//		System.out.println("n is " + n);
+//	}while(n != 0);
+	
+	
+	
+  }
+}
